e5c062e9d17be56aa00b6a006b67fa7a5f816901
Srikanth <srikanth.ganta@ixor.be>
2022-10-16 10:11:03 +0200

    Testing fail checks

--- src/main/frontend/src/App.test.js
+++ src/main/frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn reacts/i);
+  const linkElement = screen.getByText(/learn react bla bla bla/i);
   expect(linkElement).toBeInTheDocument();
 });
